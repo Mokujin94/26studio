65758ef51dfd63913a63e27a10917b5097bcbb11
mokujin <mokujinn94@gmail.com>
2023-09-27 01:26:23 +0300

    test_email_code

--- client/src/components/thirdStageReg/ThirdStageReg.jsx
+++ client/src/components/thirdStageReg/ThirdStageReg.jsx
@@ -20,35 +20,65 @@ function ThirdStageReg({ setStages }) {
 
   const onChange = (e) => {
     const id = e.target.attributes.id.value;
-    if (id === "0") {
-      setNumber1(e.target.value);
-      if (!e.target.value.length) return input1.current.focus();
-      input2.current.focus();
-    } else if (id === "1") {
-      setNumber2(e.target.value);
-      if (!e.target.value.length) return input1.current.focus();
-      console.log(e.target.value);
-      input3.current.focus();
-    } else if (id === "2") {
-      setNumber3(e.target.value);
-      if (!e.target.value.length) return input2.current.focus();
-      input4.current.focus();
-    } else if (id === "3") {
-      setNumber4(e.target.value);
-      if (!e.target.value.length) return input3.current.focus();
-      input5.current.focus();
-    } else if (id === "4") {
-      setNumber5(e.target.value);
-      if (!e.target.value.length) return input4.current.focus();
-      input6.current.focus();
-    } else if (id === "5") {
-      setNumber6(e.target.value);
-      if (!e.target.value.length) return input5.current.focus();
+    if (e.target.value.length <= 1) {
+      if (id === "1") {
+        setNumber1(e.target.value);
+        if (!e.target.value) return input1.current.focus();
+        input2.current.focus();
+      } else if (id === "2") {
+        setNumber2(e.target.value);
+        if (!e.target.value) return input1.current.focus();
+        input3.current.focus();
+      } else if (id === "3") {
+        setNumber3(e.target.value);
+        if (!e.target.value) return input2.current.focus();
+        input4.current.focus();
+      } else if (id === "4") {
+        setNumber4(e.target.value);
+        if (!e.target.value) return input3.current.focus();
+        input5.current.focus();
+      } else if (id === "5") {
+        setNumber5(e.target.value);
+        if (!e.target.value) return input4.current.focus();
+        input6.current.focus();
+      } else if (id === "6") {
+        setNumber6(e.target.value);
+        if (!e.target.value) return input5.current.focus();
+      }
     }
-    // onSwitchInput(id);
   };
-  // const onSwitchInput = (id) => {
-  //   if(id)
+
+  const onDeleteBox = (e) => {
+    const id = e.target.attributes.id.value;
+    if (e.keyCode === 8 && !e.target.value) {
+      if (id === "1") {
+      } else if (id === "2") {
+        input1.current.focus();
+      } else if (id === "3") {
+        input2.current.focus();
+      } else if (id === "4") {
+        input3.current.focus();
+      } else if (id === "5") {
+        input4.current.focus();
+      } else if (id === "6") {
+        input5.current.focus();
+      }
+    }
+
+    let nextId = Number(id) + 1;
+    if (e.target.value.length >= 1 && nextId <= 6 && e.keyCode !== 8) {
+      eval(`input${nextId}.current.focus()`);
+      eval(`setNumber${nextId}(e.key)`);
+    }
+  };
+
+  // const lastChar = (e) => {
+  //   const id = e.target.attributes.id.value;
+  //   let nextId = Number(id) + 1;
+  //   if (e.target.value.length >= 1) {
+  //     eval(`input${nextId}.current.focus()`);
+  //     eval(`setNumber${nextId}(e.key)`);
+  //   }
   // };
 
   return (
@@ -62,48 +92,55 @@ function ThirdStageReg({ setStages }) {
           <input
             type="text"
             onChange={(e) => onChange(e)}
+            onKeyDown={(e) => onDeleteBox(e)}
+            // onKeyUp={(e) => lastChar(e)}
             value={number1}
-            id="0"
+            id="1"
             ref={input1}
             className={style.third__inputBoxItem}
           />
           <input
             type="text"
             onChange={(e) => onChange(e)}
+            onKeyDown={(e) => onDeleteBox(e)}
             value={number2}
-            id="1"
+            id="2"
             ref={input2}
             className={style.third__inputBoxItem}
           />
           <input
             type="text"
             onChange={(e) => onChange(e)}
+            onKeyDown={(e) => onDeleteBox(e)}
             value={number3}
-            id="2"
+            id="3"
             ref={input3}
             className={style.third__inputBoxItem}
           />
           <input
             type="text"
             onChange={(e) => onChange(e)}
+            onKeyDown={(e) => onDeleteBox(e)}
             value={number4}
-            id="3"
+            id="4"
             ref={input4}
             className={style.third__inputBoxItem}
           />
           <input
             type="text"
             onChange={(e) => onChange(e)}
+            onKeyDown={(e) => onDeleteBox(e)}
             value={number5}
-            id="4"
+            id="5"
             ref={input5}
             className={style.third__inputBoxItem}
           />
           <input
             type="text"
             onChange={(e) => onChange(e)}
+            onKeyDown={(e) => onDeleteBox(e)}
             value={number6}
-            id="5"
+            id="6"
             ref={input6}
             className={style.third__inputBoxItem}
           />
