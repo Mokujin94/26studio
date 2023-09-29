41b30d1b9c085238f3ee0993e10d5ecd39a563cd
World Skills <WSR10SP@26kadr.internal>
2023-09-29 14:33:13 +0300

    Готовая адаптация страницы новости

--- client/src/components/newsSkeleton/NewsSkeleton.jsx
+++ client/src/components/newsSkeleton/NewsSkeleton.jsx
@@ -1,22 +1,23 @@
-import React from "react"
-import ContentLoader from "react-content-loader"
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+import style from "./newsSkeleton.module.scss";
 
 function NewsSkeleton(props) {
-    return (
-        <ContentLoader 
-            speed={2}
-            width={390}
-            height={450}
-            viewBox="0 0 390 450"
-            backgroundColor="#27323e"
-            foregroundColor="#384756"
-            {...props}
-        >
-            <rect x="0" y="0" rx="35" ry="35" width="390" height="450" /> 
-        </ContentLoader>
-        
-    );
+  return (
+    <ContentLoader
+      speed={2}
+      width={390}
+      height={450}
+      viewBox="0 0 390 450"
+      backgroundColor="#27323e"
+      foregroundColor="#384756"
+      className={style.skeleton}
+      {...props}
+    >
+      <rect x="0" y="0" rx="35" ry="35" width="390" height="450" />
+    </ContentLoader>
+  );
 }
 
-export default NewsSkeleton
-
+export default NewsSkeleton;
