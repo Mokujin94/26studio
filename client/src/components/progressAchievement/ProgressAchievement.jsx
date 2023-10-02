9e46de7bb921815526edacb3ad33148dd59c386b
LittleChapa <Daaavid943@gmail.com>
2023-10-02 23:31:44 +0300

    исправили названия компонентов наград

--- client/src/components/progressAchievement/ProgressAchievement.jsx
+++ client/src/components/progressAchievement/ProgressAchievement.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import style from './progressAchievement.module.scss'
-import achievement from '../../resource/graphics/icons/achievements/achievement.png'
-import AchievementProgress from '../achievement/AchievementProgress'
-
-
+import React from 'react';
+import style from './progressAchievement.module.scss';
+import achievement from '../../resource/graphics/icons/achievements/achievement.png';
+import ProgressAchievementItem from '../progressAchievementItem/ProgressAchievementItem';
 
 function ProgressAchievement() {
   return (
     <div className={style.progressAchievement}>
-        <div className={style.progressAchievement__info}>
-            <div className={style.progressAchievement__info__title}>Прогресс достижений</div>
-            <div className={style.progressAchievement__info__progress}>13 / 100%</div>
+      <div className={style.progressAchievement__info}>
+        <div className={style.progressAchievement__info__title}>Прогресс достижений</div>
+        <div className={style.progressAchievement__info__progress}>13 / 100%</div>
+      </div>
+      <div className={style.progressAchievement__achievement}>
+        <div className={style.mr30}>
+          <ProgressAchievementItem />
         </div>
-        <div className={style.progressAchievement__achievement}>
-            <div className={style.mr30}><AchievementProgress/></div>
-            <div className={style.mr30}><AchievementProgress/></div>
-            <div className={style.mr30}><AchievementProgress/></div>
-            <div className={style.mr30}><AchievementProgress/></div>            
+        <div className={style.mr30}>
+          <ProgressAchievementItem />
         </div>
+        <div className={style.mr30}>
+          <ProgressAchievementItem />
+        </div>
+        <div className={style.mr30}>
+          <ProgressAchievementItem />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProgressAchievement
+export default ProgressAchievement;
