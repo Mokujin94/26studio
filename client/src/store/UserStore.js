229a91f968abed0016c7f6a65117abb726b08d22
mokujin <mokujinn94@gmail.com>
2023-10-03 00:59:02 +0300

    заменена половина переменных с цветами и транзишинами в компонентах

--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -1,28 +1,33 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable } from "mobx";
 
-import news from '../resource/graphics/icons/burgerMenu/newsIcon.svg';
-import messeges from '../resource/graphics/icons/burgerMenu/messegesIcon.svg';
-import project from '../resource/graphics/icons/burgerMenu/projectIcon.svg';
-import group from '../resource/graphics/icons/burgerMenu/groupIcon.svg';
-import { GROUPS_ROUTE, MESSENGER_ROUTE, NEWS_ROUTE, PROJECTS_ROUTE } from '../utils/consts';
+import news from "../resource/graphics/icons/burgerMenu/newsIcon.svg";
+import messeges from "../resource/graphics/icons/burgerMenu/messegesIcon.svg";
+import project from "../resource/graphics/icons/burgerMenu/projectIcon.svg";
+import group from "../resource/graphics/icons/burgerMenu/groupIcon.svg";
+import {
+  GROUPS_ROUTE,
+  MESSENGER_ROUTE,
+  NEWS_ROUTE,
+  PROJECTS_ROUTE,
+} from "../utils/consts";
 
 export default class UserStore {
   constructor() {
     this._isAuth = true;
-    this._path = '';
+    this._path = "";
     this._user = {};
     this._menuAuth = [
-      { id: 0, title: 'Новости', icon: news, path: NEWS_ROUTE },
-      { id: 1, title: 'Мессенджер', icon: messeges, path: MESSENGER_ROUTE },
-      { id: 2, title: 'Проекты', icon: project, path: PROJECTS_ROUTE },
-      { id: 3, title: 'Группы', icon: group, path: GROUPS_ROUTE },
-      { id: 4, title: 'О нас', icon: messeges, path: GROUPS_ROUTE },
+      { id: 0, title: "Новости", icon: news, path: NEWS_ROUTE },
+      { id: 1, title: "Мессенджер", icon: messeges, path: MESSENGER_ROUTE },
+      { id: 2, title: "Проекты", icon: project, path: PROJECTS_ROUTE },
+      { id: 3, title: "Группы", icon: group, path: GROUPS_ROUTE },
+      { id: 4, title: "О нас", icon: messeges, path: GROUPS_ROUTE },
     ];
     this._menu = [
-      { id: 0, title: 'Новости', icon: news, path: NEWS_ROUTE },
-      { id: 1, title: 'Проекты', icon: project, path: PROJECTS_ROUTE },
-      { id: 2, title: 'Группы', icon: group, path: GROUPS_ROUTE },
-      { id: 3, title: 'О нас', icon: messeges, path: GROUPS_ROUTE },
+      { id: 0, title: "Новости", icon: news, path: NEWS_ROUTE },
+      { id: 1, title: "Проекты", icon: project, path: PROJECTS_ROUTE },
+      { id: 2, title: "Группы", icon: group, path: GROUPS_ROUTE },
+      { id: 3, title: "О нас", icon: messeges, path: GROUPS_ROUTE },
     ];
     this._burgerActive = false;
     makeAutoObservable(this);
