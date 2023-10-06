acdf8ab14c6cbc3670e9e4032186255a4d027e8e
mokujin <mokujinn94@gmail.com>
2023-10-06 20:58:16 +0300

    Ветка создана исключительно для обсуждения вопроса по поводу разработки смены темы на сайте

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,38 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { BrowserRouter, useLocation } from 'react-router-dom';
-import { ScrollContainer } from 'react-nice-scroll';
-import 'react-nice-scroll/dist/styles.css';
+import React, { useContext, useEffect, useState } from "react";
+import { BrowserRouter, useLocation } from "react-router-dom";
+import { ScrollContainer } from "react-nice-scroll";
+import "react-nice-scroll/dist/styles.css";
+import "./resource/styles/_variables.scss";
 
-import AppRouter from './components/AppRouter';
-import BurgerMenu from './components/burgerMenu/BurgerMenu';
-import Header from './components/header/Header';
+import AppRouter from "./components/AppRouter";
+import BurgerMenu from "./components/burgerMenu/BurgerMenu";
+import Header from "./components/header/Header";
 
-import './resource/styles/style.scss';
-import { observer } from 'mobx-react-lite';
-import { Context } from '.';
-import Footer from './components/footer/Footer';
-import ScrollToTop from './components/ScrollToTop';
-import { AuthProvider } from './store/AuthStore';
+import "./resource/styles/style.scss";
+import { observer } from "mobx-react-lite";
+import { Context } from ".";
+import Footer from "./components/footer/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+import { AuthProvider } from "./store/AuthStore";
 
 const App = observer(() => {
   const { user } = useContext(Context);
 
   return (
-    <div className="App">
+    <div className="App" data-theme="light">
       <BrowserRouter>
         <AuthProvider>
           <ScrollToTop />
           <BurgerMenu />
-          {user.path === '/registration' || user.path === '/login' ? null : <Header />}
+          {user.path === "/registration" || user.path === "/login" ? null : (
+            <Header />
+          )}
           <div className="App__inner">
             <AppRouter />
           </div>
-          {user.path === '/registration' || user.path === '/login' ? null : <Footer />}
+          {user.path === "/registration" || user.path === "/login" ? null : (
+            <Footer />
+          )}
         </AuthProvider>
       </BrowserRouter>
     </div>
