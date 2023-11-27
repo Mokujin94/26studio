7fe3ef10e07cf0723005df59d89eb1f37db0f3bb
Проект 26Studio <26Studio@26kadr.internal>
2023-11-27 16:14:51 +0300

    сделал анимацию в modalProject

--- client/src/components/addProjectStages/AddProjectStages.jsx
+++ client/src/components/addProjectStages/AddProjectStages.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 import style from "./addProjectStages.module.scss";
 
-function AddProjectStages({ setStages }) {
+function AddProjectStages({ stages, setStages }) {
   return (
     <div className={style.block}>
       <div className={style.block__item} onClick={() => setStages(2)}>
-        <div className={style.block__itemCircle}></div>
+        <div className={stages >= 2 ? style.block__itemCircle + " " + style.block__itemCircle_active : style.block__itemCircle}></div>
         <span className={style.block__itemTitle}>Информация</span>
       </div>
-      <span className={style.block__line}></span>
+      <span className={stages === 3 ? style.block__line + " " + style.block__line_active : style.block__line}></span>
       <div className={style.block__item} onClick={() => setStages(3)}>
-        <div className={style.block__itemCircle}></div>
+        <div className={stages === 3 ? style.block__itemCircle + " " + style.block__itemCircle_active : style.block__itemCircle}></div>
         <span className={style.block__itemTitle}>Доступность</span>
       </div>
     </div>
