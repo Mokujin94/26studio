7c67b6a461ef3dbc46bca3209b417dacf818f30a
mokujin <mokujinn94@gmail.com>
2024-01-22 00:52:48 +0300

    fix

--- client/src/http/projectAPI.js
+++ client/src/http/projectAPI.js
@@ -8,13 +8,13 @@ export const fetchProject = async (pathFromProject, baseURL) => {
   };
   const { data } = await $host.get("/api/user/accept_project", config);
   // console.log(data);
-  // let pathWithForwardSlashes = baseURL.replace(/\\/g, "/");
-  // const correctedPath = pathWithForwardSlashes.replace(/^http:\//, "http://");
+  let pathWithForwardSlashes = baseURL.replace(/\\/g, "/");
+  const correctedPath = pathWithForwardSlashes.replace(/^http:\//, "http://");
   // // console.log(pathWithForwardSlashes);
   // // Преобразуем относительные пути в абсолютные
   const transformedContent = data.replace(
     /(<head>)/,
-    (match, p1) => p1 + `<base href="${baseURL}">`
+    (match, p1) => p1 + `<base href="${correctedPath}">`
   );
 
   const updatedLinks = transformedContent.replace(
