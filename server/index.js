4a65f1d76225f3196503f40c0db967d1b4623ece
mokujin <mokujinn94@gmail.com>
2024-02-02 01:15:51 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use(
     optionsSuccessStatus: 204, // Отправлять успешный статус для OPTIONS запросов
   })
 );
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "static/news")));
 app.use(express.static(path.resolve(__dirname, "static/avatars")));
