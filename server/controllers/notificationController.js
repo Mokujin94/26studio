48519e67b4bb9321705968ce07b3b57d86a9b8f1
mokujin <mokujinn94@gmail.com>
2024-02-01 19:27:14 +0300

    fix

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const {
   Project,
   Likes,
@@ -8,53 +9,57 @@ const {
 } = require("../models/models");
 
 class NotificationController {
-
   async getAll(req, res) {
     const { recipientId } = req.query;
     const notification = await Notifications.findAll({
       include: [
-          {
-            model: Likes,
-            as: "like", // Укажите алиас, который соответствует вашей ассоциации
-          },
-          {
-            model: User,
-            as: "sender", // Укажите алиас, который соответствует вашей ассоциации
-          },
-          {
-            model: User,
-            as: "recipient", // Укажите алиас, который соответствует вашей ассоциации
-          },
-        ],
-      where: {recipientId}
-    })
+        {
+          model: Likes,
+          as: "like", // Укажите алиас, который соответствует вашей ассоциации
+        },
+        {
+          model: User,
+          as: "sender", // Укажите алиас, который соответствует вашей ассоциации
+        },
+        {
+          model: User,
+          as: "recipient", // Укажите алиас, который соответствует вашей ассоциации
+        },
+      ],
+      where: {
+        recipientId,
+        senderId: {
+          [Op.ne]: Sequelize.literal(recipientId),
+        },
+      },
+    });
     return res.json(notification);
   }
 
   async viewNotification(req, res) {
-    const { recipientId } = req.body
+    const { recipientId } = req.body;
     await Notifications.update(
       { status: true },
       { where: { recipientId, status: false } }
     );
-     const notification = await Notifications.findAll({
+    const notification = await Notifications.findAll({
       include: [
-          {
-            model: Likes,
-            as: "like", // Укажите алиас, который соответствует вашей ассоциации
-          },
-          {
-            model: User,
-            as: "sender", // Укажите алиас, который соответствует вашей ассоциации
-          },
-          {
-            model: User,
-            as: "recipient", // Укажите алиас, который соответствует вашей ассоциации
-          },
-        ],
-      where: {recipientId}
-    })
-    return res.json(notification)
+        {
+          model: Likes,
+          as: "like", // Укажите алиас, который соответствует вашей ассоциации
+        },
+        {
+          model: User,
+          as: "sender", // Укажите алиас, который соответствует вашей ассоциации
+        },
+        {
+          model: User,
+          as: "recipient", // Укажите алиас, который соответствует вашей ассоциации
+        },
+      ],
+      where: { recipientId },
+    });
+    return res.json(notification);
   }
 }
 
