ded0756985393113f119ff138c15964116e1d5c9
LittleChapa <daaavid943@gmail.com>
2024-02-02 14:21:27 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use(
     allowedHeaders: "Content-Type,Authorization",
   })
 );
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "static/news")));
 app.use(express.static(path.resolve(__dirname, "static/avatars")));
