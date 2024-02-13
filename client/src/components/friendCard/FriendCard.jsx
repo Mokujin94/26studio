be9d8a19ee7c98ff1c95714d33a2018b4709b99b
mokujin <mokujinn94@gmail.com>
2024-02-14 02:55:40 +0300

    skeletons profile

--- client/src/components/friendCard/FriendCard.jsx
+++ client/src/components/friendCard/FriendCard.jsx
@@ -10,102 +10,115 @@ import { fetchOneUser } from "../../http/userAPI";
 import { Link } from "react-router-dom";
 import { PROFILE_ROUTE } from "../../utils/consts";
 
+import FriendCardSkeleton from "../../components/Skeletons/FriendCardSkeleton";
+
 const FriendCard = ({ userId, options, onClickOne, onClickTwo }) => {
   const [user, setUser] = useState({});
   const [group, setGroup] = useState();
+  const [isLoadingFriend, setIsLoadingFriend] = useState(true);
 
   useEffect(() => {
+    setIsLoadingFriend(true);
     fetchOneUser(userId)
       .then((data) => {
         setUser(data);
+        setIsLoadingFriend(false);
       })
       .catch((err) => console.log(err));
   }, []);
   return (
-    <Link to={PROFILE_ROUTE + "/" + userId} className={style.friendCard}>
-      <div className={style.friendCard__avatar}>
-        <img
-          src={process.env.REACT_APP_API_URL + user.avatar}
-          alt="img"
-          className={style.friendCard__avatar__img}
-        />
-      </div>
-      <div className={style.friendCard__info}>
-        <h2 className={style.friendCard__infoNickName}>{user.name}</h2>
-        <h2 className={style.friendCard__infoFullname}>{user.full_name}</h2>
-        <h2 className={style.friendCard__info__online}>был в сети: 4ч назад</h2>
-      </div>
-      {options && (
-        <div className={style.friendCard__buttons}>
-          {options === 1 ? (
-            <>
-              <div
-                className={style.friendCard__buttons__item}
-                onClick={onClickTwo}
-              >
-                <svg
-                  className={style.friendCard__buttons__item__icon}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="25"
-                  height="25"
-                  viewBox="0 0 25 25"
-                  fill="none"
-                >
-                  <circle cx="12.5" cy="12.5" r="12" stroke="white" />
-                  <path
-                    d="M7.69427 5L5 7.69427L6.3758 9.07006L9.77707 12.5287L6.3758 15.9299L5 17.2484L7.69427 20L9.07006 18.6242L12.5287 15.1656L15.9299 18.6242L17.2484 20L20 17.2484L18.6242 15.9299L15.1656 12.5287L18.6242 9.07006L20 7.69427L17.2484 5L15.9299 6.3758L12.5287 9.77707L9.07006 6.3758L7.69427 5Z"
-                    fill="#FCFCFC"
-                  />
-                </svg>
-              </div>
-            </>
-          ) : (
-            options === 2 && (
-              <>
-                <div
-                  className={style.friendCard__buttons__item}
-                  onClick={onClickTwo}
-                >
-                  <svg
-                    className={style.friendCard__buttons__item__icon}
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="25"
-                    height="25"
-                    viewBox="0 0 25 25"
-                    fill="none"
+    <>
+      {isLoadingFriend ? (
+        <FriendCardSkeleton />
+      ) : (
+        <Link to={PROFILE_ROUTE + "/" + userId} className={style.friendCard}>
+          <div className={style.friendCard__avatar}>
+            <img
+              src={process.env.REACT_APP_API_URL + user.avatar}
+              alt="img"
+              className={style.friendCard__avatar__img}
+            />
+          </div>
+          <div className={style.friendCard__info}>
+            <h2 className={style.friendCard__infoNickName}>{user.name}</h2>
+            <h2 className={style.friendCard__infoFullname}>{user.full_name}</h2>
+            <h2 className={style.friendCard__info__online}>
+              был в сети: 4ч назад
+            </h2>
+          </div>
+          {options && (
+            <div className={style.friendCard__buttons}>
+              {options === 1 ? (
+                <>
+                  <div
+                    className={style.friendCard__buttons__item}
+                    onClick={onClickTwo}
                   >
-                    <circle cx="12.5" cy="12.5" r="12" stroke="white" />
-                    <path
-                      d="M7.69427 5L5 7.69427L6.3758 9.07006L9.77707 12.5287L6.3758 15.9299L5 17.2484L7.69427 20L9.07006 18.6242L12.5287 15.1656L15.9299 18.6242L17.2484 20L20 17.2484L18.6242 15.9299L15.1656 12.5287L18.6242 9.07006L20 7.69427L17.2484 5L15.9299 6.3758L12.5287 9.77707L9.07006 6.3758L7.69427 5Z"
-                      fill="#FCFCFC"
-                    />
-                  </svg>
-                </div>
-                <div
-                  className={style.friendCard__buttons__item}
-                  onClick={onClickOne}
-                >
-                  <svg
-                    className={style.friendCard__buttons__item__icon}
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="25"
-                    height="25"
-                    viewBox="0 0 25 25"
-                    fill="none"
-                  >
-                    <circle cx="12.5" cy="12.5" r="12" stroke="white" />
-                    <path
-                      d="M17.2484 7L15.9299 8.13386L10.6178 12.5118L9.07006 11.2835L7.69427 10.1496L5 12.3701L6.3758 13.5039L9.24204 15.8661L10.5605 17L11.9363 15.8661L18.6242 10.3543L20 9.22047L17.2484 7Z"
-                      fill="#FCFCFC"
-                    />
-                  </svg>
-                </div>
-              </>
-            )
+                    <svg
+                      className={style.friendCard__buttons__item__icon}
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      height="25"
+                      viewBox="0 0 25 25"
+                      fill="none"
+                    >
+                      <circle cx="12.5" cy="12.5" r="12" stroke="white" />
+                      <path
+                        d="M7.69427 5L5 7.69427L6.3758 9.07006L9.77707 12.5287L6.3758 15.9299L5 17.2484L7.69427 20L9.07006 18.6242L12.5287 15.1656L15.9299 18.6242L17.2484 20L20 17.2484L18.6242 15.9299L15.1656 12.5287L18.6242 9.07006L20 7.69427L17.2484 5L15.9299 6.3758L12.5287 9.77707L9.07006 6.3758L7.69427 5Z"
+                        fill="#FCFCFC"
+                      />
+                    </svg>
+                  </div>
+                </>
+              ) : (
+                options === 2 && (
+                  <>
+                    <div
+                      className={style.friendCard__buttons__item}
+                      onClick={onClickTwo}
+                    >
+                      <svg
+                        className={style.friendCard__buttons__item__icon}
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="25"
+                        height="25"
+                        viewBox="0 0 25 25"
+                        fill="none"
+                      >
+                        <circle cx="12.5" cy="12.5" r="12" stroke="white" />
+                        <path
+                          d="M7.69427 5L5 7.69427L6.3758 9.07006L9.77707 12.5287L6.3758 15.9299L5 17.2484L7.69427 20L9.07006 18.6242L12.5287 15.1656L15.9299 18.6242L17.2484 20L20 17.2484L18.6242 15.9299L15.1656 12.5287L18.6242 9.07006L20 7.69427L17.2484 5L15.9299 6.3758L12.5287 9.77707L9.07006 6.3758L7.69427 5Z"
+                          fill="#FCFCFC"
+                        />
+                      </svg>
+                    </div>
+                    <div
+                      className={style.friendCard__buttons__item}
+                      onClick={onClickOne}
+                    >
+                      <svg
+                        className={style.friendCard__buttons__item__icon}
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="25"
+                        height="25"
+                        viewBox="0 0 25 25"
+                        fill="none"
+                      >
+                        <circle cx="12.5" cy="12.5" r="12" stroke="white" />
+                        <path
+                          d="M17.2484 7L15.9299 8.13386L10.6178 12.5118L9.07006 11.2835L7.69427 10.1496L5 12.3701L6.3758 13.5039L9.24204 15.8661L10.5605 17L11.9363 15.8661L18.6242 10.3543L20 9.22047L17.2484 7Z"
+                          fill="#FCFCFC"
+                        />
+                      </svg>
+                    </div>
+                  </>
+                )
+              )}
+            </div>
           )}
-        </div>
+        </Link>
       )}
-    </Link>
+    </>
   );
 };
 
