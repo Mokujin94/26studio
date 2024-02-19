703bca84bc9020826aaadf5aa5febef434444329
LittleChapa <Daaavid943@gmail.com>
2024-02-20 00:13:16 +0300

    profile update (setting projects)

--- client/src/pages/news/News.jsx
+++ client/src/pages/news/News.jsx
@@ -11,53 +11,54 @@ import { fetchNews } from "../../http/newsAPI";
 import { observer } from "mobx-react-lite";
 
 const News = observer(() => {
-  const { news } = useContext(Context);
-  const [isLoaded, setIsLoaded] = useState(true);
-  const [newsCollection, setNewsCollection] = useState();
+	const { news } = useContext(Context);
+	const [isLoaded, setIsLoaded] = useState(true);
+	const [newsCollection, setNewsCollection] = useState();
 
-  useEffect(() => {
-    fetchNews()
-      .then((data) => {
-        news.setNews(data.rows);
-        setNewsCollection(
-          news.news.map((item) => (
-            <Link key={item.id} to={NEWSPAPER_ROUTE + "/" + item.id}>
-              <NewsCard news={item} />
-            </Link>
-          ))
-        );
-        setIsLoaded(false);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+	useEffect(() => {
+		document.title = "Новости";
+		fetchNews()
+			.then((data) => {
+				news.setNews(data.rows);
+				setNewsCollection(
+					news.news.map((item) => (
+						<Link key={item.id} to={NEWSPAPER_ROUTE + "/" + item.id}>
+							<NewsCard news={item} />
+						</Link>
+					))
+				);
+				setIsLoaded(false);
+			})
+			.catch((err) => console.log(err));
+	}, []);
 
-  const skeletonList = [
-    { id: 0 },
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { id: 7 },
-  ];
+	const skeletonList = [
+		{ id: 0 },
+		{ id: 1 },
+		{ id: 2 },
+		{ id: 3 },
+		{ id: 4 },
+		{ id: 5 },
+		{ id: 6 },
+		{ id: 7 },
+	];
 
-  const newSkeletonList = skeletonList.map(({ id }) => (
-    <NewsSkeleton key={id} />
-  ));
+	const newSkeletonList = skeletonList.map(({ id }) => (
+		<NewsSkeleton key={id} />
+	));
 
-  return (
-    <div className="container">
-      <div className="news">
-        <div className="news__title-wrapper">
-          <h1 className="news__title">Последние новости</h1>
-        </div>
-        <div className="news__cards">
-          {isLoaded ? newSkeletonList : newsCollection}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="news">
+				<div className="news__title-wrapper">
+					<h1 className="news__title">Последние новости</h1>
+				</div>
+				<div className="news__cards">
+					{isLoaded ? newSkeletonList : newsCollection}
+				</div>
+			</div>
+		</div>
+	);
 });
 
 export default News;
