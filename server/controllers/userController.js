28aeda8448f10de1cfe70e03db63cbbca50c8a2d
mokujin <mokujinn94@gmail.com>
2024-02-21 22:37:53 +0300

    fix cors

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -299,6 +299,10 @@ class UserController {
 	}
 
 	async uploadProject(req, res, next) {
+		res.setHeader('Access-Control-Allow-Origin', 'https://poetic-halva-67c56b.netlify.app');
+		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+		res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+		res.setHeader('Access-Control-Allow-Credentials', 'true');
 		try {
 			const uploadPath = path.join(__dirname, "..", "uploads");
 			const extractPath = path.join(__dirname, "..", "extracted");
@@ -376,11 +380,17 @@ class UserController {
 					// Пропускаем содержимое файла
 					entry.autodrain();
 				});
-
+				res.setHeader('Access-Control-Allow-Origin', 'https://poetic-halva-67c56b.netlify.app');
+				res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+				res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+				res.setHeader('Access-Control-Allow-Credentials', 'true');
 				extractStream.on("finish", () => {
 					// Выводим список относительных путей файлов
 					console.log("Files in the archive:", filePaths);
-
+					res.setHeader('Access-Control-Allow-Origin', 'https://poetic-halva-67c56b.netlify.app');
+					res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+					res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+					res.setHeader('Access-Control-Allow-Credentials', 'true');
 					res.json({ filePaths, normalPath, baseUrl });
 				});
 
