2ef2de7d80740d26637b815cba5312664b5db5ae
mokujin <mokujinn94@gmail.com>
2024-03-09 23:39:00 +0300

    test save files on server

--- client/src/http/viewAPI.js
+++ client/src/http/viewAPI.js
@@ -1,17 +1,19 @@
 import { $authHost, $host, $adminHost } from "./index";
 
 export const viewProject = async (projectId, userId) => {
-  const { data } = await $authHost.post("api/view/project", {
-    projectId,
-    userId,
-  });
-  return data;
+	const { data } = await $authHost.post("api/view/project", {
+		projectId,
+		userId,
+	});
+	return data;
 };
 
 export const viewNews = async (newsId, userId) => {
-  const { data } = await $authHost.post("api/view/news", {
-    newsId,
-    userId,
-  });
-  return data;
+	const { data } = await $authHost.post("api/view/news", {
+		newsId,
+		userId,
+	});
+	return data;
 };
+
+
