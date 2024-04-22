c4938a3fe150f932cf7ba17bbaea0b1b31982724
LittleChapa <daaavid943@gmail.com>
2024-04-22 19:16:35 +0300

    Начата верстка мессенджера

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -4,10 +4,8 @@ import MessengerSideBar from '../../components/messengerSideBar/MessengerSideBar
 function Messenger() {
 	return (
 		<div className='messenger'>
-			<div className="container container_messenger">
-				<div className="messenger__inner">
-					<MessengerSideBar />
-				</div>
+			<div className="messenger__inner">
+				<MessengerSideBar />
 			</div>
 		</div>
 	)
