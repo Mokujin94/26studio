f3f8332d98f439152f5682217c8ac57e707f903d
LittleChapa <daaavid943@gmail.com>
2024-05-04 16:49:13 +0300

    убрана анимация у сообщений

--- client/src/components/message/Message.jsx
+++ client/src/components/message/Message.jsx
@@ -14,40 +14,19 @@ const Message = observer(({ messages }) => {
 				<img src={!!messages.length && process.env.REACT_APP_API_URL + "/" + messages[0].user.avatar} alt="" />
 			</div>
 			<div className={style.message__list}>
-				<TransitionGroup component={null}>
-					{
-						messages.map(message => {
-							if (message.userId == user.user.id) {
-
-								return (
-									<CSSTransition
-										key={message.id}
-										timeout={200} // Время анимации в миллисекундах
-										classNames="message" // Префикс классов для анимации
-									>
-										<div className={style.message__list_item}>
-											<MessageContent content={message.text} isOther={false} />
-										</div>
-									</CSSTransition>
-								)
-							} else {
-								return (
-									<CSSTransition
-										key={message.id}
-										timeout={200} // Время анимации в миллисекундах
-										classNames="message" // Префикс классов для анимации
-									>
-
-										<div className={style.message__list_item + ' ' + style.message__list_item_other}>
-											<MessageContent content={message.text} isOther={true} />
-										</div>
-									</CSSTransition>
-								)
-							}
-							console.log(message)
-						})
-					}
-				</TransitionGroup>
+				{
+					messages.map(message => {
+						if (message.userId == user.user.id) {
+							return (
+								<MessageContent content={message.text} isOther={false} />
+							)
+						} else {
+							return (
+								<MessageContent content={message.text} isOther={true} />
+							)
+						}
+					})
+				}
 			</div>
 		</div >
 	)
