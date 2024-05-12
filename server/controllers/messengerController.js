ae3e9db96dfab3580c8af73b0e6e5429fd1254fa
mokujin <mokujinn94@gmail.com>
2024-05-12 03:18:54 +0300

    fix

--- server/controllers/messengerController.js
+++ server/controllers/messengerController.js
@@ -63,7 +63,8 @@ class MessengerController {
 		function isSameDay(date1, date2) {
 			return date1.getDate() === date2.getDate() &&
 				date1.getMonth() === date2.getMonth() &&
-				date1.getFullYear() === date2.getFullYear();
+				date1.getFullYear() === date2.getFullYear() &&
+				date1.getHours() === date2.getHours();
 		}
 
 		function groupMessagesByUser(messages) {
