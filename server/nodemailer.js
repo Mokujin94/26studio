8d6c75f55e920c0671bc3fd459f6916fbce89065
mokujin <mokujinn94@gmail.com>
2025-08-17 14:40:53 +0300

    test

--- server/nodemailer.js
+++ server/nodemailer.js
@@ -1,25 +1,28 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport(
-    {
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            user: "26studio.college@gmail.com",
-            pass: "jmxbxygojrrprmew",
-        },
-    },
-    {
-        from: "26Studio <26studio.college@gmail.com>",
-    }
+	{
+		host: "smtp.gmail.com",
+		port: 465,
+		secure: true,
+		auth: {
+			user: "26studio.college@gmail.com",
+			pass: "jmxbxygojrrprmew",
+		},
+	},
+	{
+		from: "26Studio <26studio.college@gmail.com>",
+	}
 );
 
 const mailer = (message) => {
-    transporter.sendMail(message, (err, info) => {
-        if (err) return
-
-    });
+	transporter.sendMail(message, (err, info) => {
+		if (err) {
+			console.error("MAIL ERROR:", err); // <-- логируем
+			return;
+		}
+		console.log("MAIL OK:", info.response);
+	});
 };
 
 module.exports = mailer;
